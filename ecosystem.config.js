0c34fd37ec93ab7e09e3e45671db6d4e0a89e51f
Ubuntu <ubuntu@ip-172-31-47-222.us-west-2.compute.internal>
2018-09-21 01:53:02 +0000

    New server config.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	deploy: {
 		production: {
 			user: 'ubuntu',
-			host: 'ec2-54-201-206-134.us-west-2.compute.amazonaws.com',
+			host: 'ec2-35-160-3-32.us-west-2.compute.amazonaws.com',
 			key: '~/.ssh/webserver.pem',
 			ref: 'origin/master',
 			repo: 'git@github.com:sprocketjockey/fitting-exploder.git',
